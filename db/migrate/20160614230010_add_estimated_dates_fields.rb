7e7f7caf378d7514918df96e03d4a2951b47028c
tania daniela paiva <taniadaniela1803@gmail.com>
2016-06-14 19:29:53 -0400

    Added migration for actual due date and actual start date.

---
+++ db/migrate/20160614230010_add_estimated_dates_fields.rb
@@ -0,0 +1,11 @@
+class AddEstimatedDatesFields < ActiveRecord::Migration
+  def self.up
+    add_column :issues, :actual_end_date, :date
+    add_column :issues, :actual_start_date, :date
+  end
+
+  def self.down
+    remove_column :issues, :actual_due_date
+    remove_column :issues, :actual_start_date
+  end
+end
