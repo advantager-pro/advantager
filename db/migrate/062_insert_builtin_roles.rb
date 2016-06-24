bbc24bd3d3e17240f88b056431ad7be4094716e2
tania daniela paiva <taniadaniela1803@gmail.com>
2016-06-23 23:12:27 -0400

    Commented initial default roles.

--- db/migrate/062_insert_builtin_roles.rb
+++ db/migrate/062_insert_builtin_roles.rb
@@ -1,13 +1,13 @@
 class InsertBuiltinRoles < ActiveRecord::Migration
   def self.up
-    Role.reset_column_information
-    nonmember = Role.new(:name => 'Non member', :position => 0)
-    nonmember.builtin = Role::BUILTIN_NON_MEMBER
-    nonmember.save
-
-    anonymous = Role.new(:name => 'Anonymous', :position => 0)
-    anonymous.builtin = Role::BUILTIN_ANONYMOUS
-    anonymous.save
+    # Role.reset_column_information
+    # nonmember = Role.new(:name => 'Non member', :position => 0)
+    # nonmember.builtin = Role::BUILTIN_NON_MEMBER
+    # nonmember.save
+    #
+    # anonymous = Role.new(:name => 'Anonymous', :position => 0)
+    # anonymous.builtin = Role::BUILTIN_ANONYMOUS
+    # anonymous.save
   end
 
   def self.down
