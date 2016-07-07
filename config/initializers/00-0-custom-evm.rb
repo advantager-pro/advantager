ecc997ea4d92e9d66594feacdb7845c7de083639
Edipo Vinicius da Silva <edipox@gmail.com>
2016-07-07 13:14:35 -0400

    Comments to stub Date.today method added to initializer:

--- config/initializers/00-0-custom-evm.rb
+++ config/initializers/00-0-custom-evm.rb
@@ -1,2 +1,8 @@
 Dir["#{Rails.root}/lib/custom/models/*.rb"].each {|file| require file }
 Dir["#{Rails.root}/lib/custom/models/evm/*.rb"].each {|file| require file }
+# 
+# Date.class_eval do
+#   def self.today
+#     Time.now.to_date - 1.days
+#   end
+# end
