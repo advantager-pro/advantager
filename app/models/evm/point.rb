3992cc4e69a4512d33f9da4e601570c8459a4097
Edipo Vinicius da Silva <edipox@gmail.com>
2016-07-07 13:13:02 -0400

    added points logic

--- app/models/evm/point.rb
+++ app/models/evm/point.rb
@@ -2,16 +2,28 @@ class Evm::Point < ActiveRecord::Base
   belongs_to :issue
   validates_presence_of :issue
 
+  attr_accessible :project_id
+
+  def project
+    ::Project.find_by(id: project_id)
+    # We cannot use issue.project because of the grouped_by_day
+  end
+
+  include EVM::Methods
+
   def self.update_current_point!(issue)
     Evm::Point.find_or_initialize_by(issue: issue,
-      date: Date.today).save!
+      day: Date.today).save!
   end
 
   def self.grouped_by_day(project)
+    tbname = self.table_name
+    # extra_fields = self.column_names - %w( planned_value actual_cost earned_value )
+    # extra_fields = extra_fields.map{ |c| "#{tbname}.#{c}" }.join(", ")
     # http://stackoverflow.com/questions/9068926/rails-3-group-by-and-sum
-    project.points.select("SUM(evm_points.planned_value) as planned_value,
-      SUM(evm_points.actual_cost) as actual_cost
-      SUM(evm_points.earned_value) as earned_value").group("evm_points.day")
+    project.evm_points.select("#{tbname}.day, #{::Issue.table_name}.project_id, SUM(#{tbname}.planned_value) as planned_value,
+      SUM(#{tbname}.actual_cost) as actual_cost,
+      SUM(#{tbname}.earned_value) as earned_value").group("#{tbname}.day, #{::Issue.table_name}.project_id")
   end
 
   before_save do
