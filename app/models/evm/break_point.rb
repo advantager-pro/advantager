d38f74c185937038056d3fc433e8f2e0072de62c
Edipo Vinicius da Silva <edipox@gmail.com>
2016-07-10 16:11:14 -0400

    Date fixnum conversion issue fixed in EVM break point model

--- app/models/evm/break_point.rb
+++ app/models/evm/break_point.rb
@@ -7,21 +7,22 @@ class Evm::BreakPoint < ActiveRecord::Base
   end
 
   def self.create_minimum(project, last_date)
-    min = EVM::BreakPoint.minimum_quantity_for_feature
+    min = Evm::BreakPoint.minimum_quantity_for_feature
     self.create_for_project(project, last_date, min)
   end
 
   def self.create_for_project(project, last_date, qtty)
-    bps = qtty.times.map{ |i| { project: project, date: (i*project.evm_frequency).days+last_date } }
+    bps = qtty.times.map {|i| { project: project,
+      day: last_date+((i*project.evm_frequency).days) } }
     Evm::BreakPoint.create!(bps)
   end
 
   def self.generate_until(project, date)
     bps = []
-    last_date = project.created_at
+    last_date = project.created_on
     freq = project.evm_frequency.days
     until last_date > date
-      bps <<  { project: project, date: last_date }
+      bps <<  { project: project, day: last_date }
       last_date += freq
     end
     Evm::BreakPoint.create!(bps)
@@ -40,6 +41,6 @@ class Evm::BreakPoint < ActiveRecord::Base
 
   # The BreakPoint should be updated if it is a BreakPoint day
   after_update do
-    create_minimum(project, date)
+    create_minimum(project, Date.today)
   end
 end
