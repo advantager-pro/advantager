4a583309eab74d27dee8a5560002a94378b4eef7
Edipo Vinicius da Silva <edipox@gmail.com>
2016-07-14 21:00:01 -0400

    Helper to display json data for morris added

--- app/helpers/evm_helper.rb
+++ app/helpers/evm_helper.rb
@@ -28,4 +28,38 @@ module EVMHelper
     end.join(',').html_safe + ']'
   end
 
+  def data_for_morris(evm_points, options)
+    fields = options[:fields]
+    pts = evm_points.map do |e|
+      hash = {}
+      fields.each do |field|
+        if field == 'day'
+            d = e.day
+            hash[field] = "#{d.year}-#{d.month}-#{d.day}"
+        else
+          hash[field] = e.send(field)
+        end
+      end
+      hash
+    end
+    if fields.include?('planned_value') || fields.include?(:planned_value)
+      lp = evm_points.last
+      d = lp.project.due_date
+      pts << { planned_value: lp.planned_value, day: "#{d.year}-#{d.month}-#{d.day}" }
+    end
+    pts.to_json.html_safe
+  end
+
+  def evm_break_points_events(project)
+    bps = []
+    current_date = project.created_on
+    last_date = project.due_date
+    freq = project.evm_frequency.days
+    while last_date > current_date
+      bps <<  current_date
+      current_date += freq
+    end
+    bps.to_json.html_safe
+  end
+
 end
