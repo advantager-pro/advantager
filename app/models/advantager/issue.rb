5e3d1c5c54afb2da1240efe78cd7e30dc66bbb93
Edipo Vinicius da Silva <edipox@gmail.com>
2016-07-21 21:38:06 -0400

    Fix status closed transition

--- app/models/advantager/issue.rb
+++ app/models/advantager/issue.rb
@@ -3,12 +3,18 @@ module Advantager::Issue
 
     included do
 
+      def in_progress_done_ratio
+        ::IssueStatus.find_in_progress_status.default_done_ratio || 0
+      end
+
       def done_ratio_in_progress?
-        in_progress_done_ratio = ::IssueStatus.find_in_progress_status.default_done_ratio
-        done_ratio >= (in_progress_done_ratio || 0)
+        return false if done_ratio.nil?
+        done_ratio >= in_progress_done_ratio &&
+        done_ratio > 0
       end
 
       def done_ratio_closed?
+        return false if done_ratio.nil?
         done_ratio == 100
       end
 
@@ -18,21 +24,23 @@ module Advantager::Issue
         return unless (status_was.present? && self.status != status_was)
         if was_closed? && status.in_progress?
           self.actual_due_date = nil
-          self.done_ratio = ::IssueStatus.find_in_progress_status.default_done_ratio
+          self.done_ratio = in_progress_done_ratio
         elsif status_was.initial? && status.in_progress?
           actual_start_date = Date.today
-          done_ratio = ::IssueStatus.find_in_progress_status.default_done_ratio
+          done_ratio = in_progress_done_ratio
+        elsif status.is_closed?
+          self.actual_due_date = ::Date.today if self.actual_due_date.nil?
+          self.done_ratio = 100
         end
       end
 
       def binding_actual_dates
         return unless actual_start_date_changed? ||
           actual_due_date_changed?
-        if self.actual_due_date <= ::Date.today
-          in_progress_status = ::IssueStatus.find_in_progress_status
-          self.status = in_progress_status if status.initial?
-          self.done_ratio = in_progress_status.default_done_ratio
-        elsif self.actual_due_date <= ::Date.today
+        if self.actual_due_date.present? && self.actual_due_date <= ::Date.today
+          self.status = ::IssueStatus.find_in_progress_status if status.initial?
+          self.done_ratio = in_progress_done_ratio
+        elsif self.actual_due_date.present? && self.actual_due_date <= ::Date.today
           closed_status = ::IssueStatus.where(is_closed: true).first
           self.status = closed_status
           self.done_ratio = 100
@@ -41,13 +49,13 @@ module Advantager::Issue
 
       def binding_done_ratio
         return unless done_ratio_changed?
-        if done_ratio_in_progress?
-          self.status = ::IssueStatus.find_in_progress_status if status.initial?
-          self.actual_start_date = ::Date.today if self.actual_start_date.nil?
-        elsif done_ratio_closed?
+        if done_ratio_closed?
           closed_status = ::IssueStatus.where(is_closed: true).first
           self.status = closed_status
           self.actual_due_date = ::Date.today if self.actual_due_date.nil?
+        elsif done_ratio_in_progress?
+          self.status = ::IssueStatus.find_in_progress_status if status.initial?
+          self.actual_start_date = ::Date.today if self.actual_start_date.nil?
         end
       end
 
