8452097b8d98706cef9cc23e97373c7454a024c3
Edipo Vinicius da Silva <edipox@gmail.com>
2016-07-27 20:32:06 -0400

    Remove cache from EVM project methods

--- app/models/evm/project_methods.rb
+++ app/models/evm/project_methods.rb
@@ -4,18 +4,15 @@ module EVM::ProjectMethods
     include ::EVM::Methods
     included do
       def _budget_at_conclusion
-        Rails.cache.fetch("#{cache_key}/budget_at_conclusion", expires_in: 5.minutes) do
           sum = 0.0
           endate = self.due_date || (Date.today + 1.day)
           # TODO: use SUM here. In the other methods you cannot
           #   use SUM because they use the date param
           issues.not_rejected.planned_only.each{ |e| sum += e.planned_value(endate) }
           sum
-        end
       end
 
       def planned_value(date=nil)
-        # Rails.cache.fetch("#{cache_key}/planned_value", expires_in: 5.minutes) do
           sum = 0.0
           # We don't need issues that we didn't planned to start
           if date.nil?
@@ -25,29 +22,24 @@ module EVM::ProjectMethods
               date).each{ |e| sum += e.planned_value(date) }
           end
           sum
-        # end
       end
 
       def actual_cost(date=nil)
-        # Rails.cache.fetch("#{cache_key}/actual_cost", expires_in: 5.minutes) do
           sum = 0.0
           date ||= Date.today
           # We only need issues that are in progress or finished
           issues.not_rejected.planned_only.where("#{::Issue.table_name}.actual_start_date <= ?",
             date).each{ |e| sum += e.actual_cost(date) }
           sum
-        # end
       end
 
       def earned_value(date=nil)
-        # Rails.cache.fetch("#{cache_key}/earned_value", expires_in: 5.minutes) do
           sum = 0.0
           date ||= Date.today
           # We only need issues that are in progress or finished
           issues.not_rejected.planned_only.where("#{::Issue.table_name}.actual_start_date <= ?",
             date).each{ |e| sum += e.earned_value(date) }
           sum
-        # end
       end
 
       # validates :evm_field, presence: true, inclusion: { in: ::Project.available_fields }
