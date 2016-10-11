2b4a11328e371116f44d267eef03a110a9184503
tania daniela paiva <taniadaniela1803@gmail.com>
2016-10-10 21:03:45 -0300

    Added seeds for priorities.

---
+++ db/fixtures/priorities.rb
@@ -0,0 +1,20 @@
+#Default priorities
+low = IssuePriority.seed(:id,
+  {id: 1, name: "Baja", position:1, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "lowest"}).first
+
+nor = IssuePriority.seed(:id,
+  {id: 2, name: "Normal", position:2, is_default: true, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "Default"}).first
+
+hi = IssuePriority.seed(:id,
+  {id: 3, name: "Alta", position:3, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "high"}).first
+
+cri =IssuePriority.seed(:id,
+  {id: 4, name: "Critica", position:4, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "highest"}).first
+
+priorities = {default_priority_high: hi, default_priority_low: low, default_priority_critical: cri, default_priority_normal: nor}
+priorities.each do |translation, model|
+  I18n.available_locales.each  do |loc|
+    model.attributes = {name: I18n.t!(translation, locale: loc), locale: loc}
+  end
+  model.save!
+end
