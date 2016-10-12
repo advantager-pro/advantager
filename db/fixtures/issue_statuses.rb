b264ed3c0a8e324f58f108b3ae2a001fce6fac0f
tania daniela paiva <taniadaniela1803@gmail.com>
2016-10-11 22:16:49 -0300

    Using translations for required name on trackers and statuses.

--- db/fixtures/issue_statuses.rb
+++ db/fixtures/issue_statuses.rb
@@ -1,15 +1,11 @@
 # #Default Issue status
-default_issue_status = IssueStatus.seed(:id,
-  {id: 1, name: "Nueva", is_closed: false, position: 1, default_done_ratio: nil}).first
+default_issue_status = IssueStatus.seed(:id, {id: 1, name: I18n.t!("default_issue_status_new"), is_closed: false, position: 1, default_done_ratio: nil}).first
 
-pro =IssueStatus.seed(:id,
-  {id: 2, name: "En progreso", is_closed: false, position: 2, default_done_ratio: nil}).first
+pro =IssueStatus.seed(:id,{id: 2, name: I18n.t!("default_issue_status_in_progress"), is_closed: false, position: 2, default_done_ratio: nil}).first
 
-clo = IssueStatus.seed(:id,
-  {id: 3, name: "Cerrada", is_closed: true, position: 3, default_done_ratio: 100}).first
+clo = IssueStatus.seed(:id, {id: 3, name: I18n.t!("default_issue_status_closed"), is_closed: true, position: 3, default_done_ratio: 100}).first
 
-rej = IssueStatus.seed(:id,
-  {id: 4, name: "Rechazada", is_closed: false, position: 4, default_done_ratio: nil}).first
+rej = IssueStatus.seed(:id, {id: 4, name: I18n.t!("default_issue_status_rejected"), is_closed: false, position: 4, default_done_ratio: nil}).first
 
 statuses = {default_issue_status_new: default_issue_status, default_issue_status_in_progress: pro, default_issue_status_closed: clo, default_issue_status_rejected: rej}
 statuses.each do |translation, model|
--- db/fixtures/issue_trackers.rb
+++ db/fixtures/issue_trackers.rb
@@ -2,10 +2,10 @@
 default_issue_status = IssueStatus.where(name: I18n.t!("default_issue_status_new")).first
 
 mt = Tracker.seed(:id,
-  {id: 1,name: "Hito", position: 1, is_in_roadmap: true, default_status_id: default_issue_status.id}).first
+  {id: 1,name: I18n.t!("default_tracker_milestone"), position: 1, is_in_roadmap: true, default_status_id: default_issue_status.id}).first
 
 ts = Tracker.seed(:id,
-  {id: 2,name: "Tarea", position: 2, is_in_roadmap: true, default_status_id: default_issue_status.id}).first
+  {id: 2,name: I18n.t!("default_tracker_task"), position: 2, is_in_roadmap: true, default_status_id: default_issue_status.id}).first
 
 trackers = {default_tracker_task: ts, default_tracker_milestone: mt}
 trackers.each do |translation, model|
