af952f6547a386bb6d64a7deb4da36abe98527ed
tania daniela paiva <taniadaniela1803@gmail.com>
2016-10-19 23:00:16 -0300

    Added missing I18n for issue priorities seed

--- db/fixtures/priorities.rb
+++ db/fixtures/priorities.rb
@@ -1,15 +1,15 @@
 #Default priorities
 low = IssuePriority.seed(:id,
-  {id: 1, name: "Baja", position:1, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "lowest"}).first
+  {id: 1, name: I18n.t!("default_priority_low"), position:1, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "lowest"}).first
 
 nor = IssuePriority.seed(:id,
-  {id: 2, name: "Normal", position:2, is_default: true, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "Default"}).first
+  {id: 2, name: I18n.t!("default_priority_normal"), position:2, is_default: true, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "Default"}).first
 
 hi = IssuePriority.seed(:id,
-  {id: 3, name: "Alta", position:3, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "high"}).first
+  {id: 3, name: I18n.t!("default_priority_high"), position:3, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "high"}).first
 
 cri =IssuePriority.seed(:id,
-  {id: 4, name: "Critica", position:4, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "highest"}).first
+  {id: 4, name: I18n.t!("default_priority_critical"), position:4, type: "IssuePriority", active: true, project_id: nil, parent_id: nil, position_name: "highest"}).first
 
 priorities = {default_priority_high: hi, default_priority_low: low, default_priority_critical: cri, default_priority_normal: nor}
 priorities.each do |translation, model|
