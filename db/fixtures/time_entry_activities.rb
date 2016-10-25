1f42a0e9041a563f19adc9d38b88628bc16380a1
tania daniela paiva <taniadaniela1803@gmail.com>
2016-10-24 22:11:56 -0300

    Fixed issue with time entry activity seed.

--- db/fixtures/time_entry_activities.rb
+++ db/fixtures/time_entry_activities.rb
@@ -1,21 +1,21 @@
 #Default TimeEntryActivity
 pl = TimeEntryActivity.seed(:id,
-  {id: 1,name: I18n.t!("default_activity_planification"), position: 1, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
+  {id: nil ,name: I18n.t!("default_activity_planification"), position: 1, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
 
 dev = TimeEntryActivity.seed(:id,
-  {id: 2,name: I18n.t!("default_activity_development"), position: 2, is_default: true, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
+  {id: nil,name: I18n.t!("default_activity_development"), position: 2, is_default: true, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
 
 inv = TimeEntryActivity.seed(:id,
-  {id: 3,name: I18n.t!("default_activity_investigation"), position: 3, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
+  {id: nil,name: I18n.t!("default_activity_investigation"), position: 3, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
 
 tst = TimeEntryActivity.seed(:id,
-  {id: 4,name: I18n.t!("default_activity_testing"), position: 4, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
+  {id: nil,name: I18n.t!("default_activity_testing"), position: 4, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
 
 doc = TimeEntryActivity.seed(:id,
-  {id: 5,name: I18n.t!("default_activity_documentation"), position: 5, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
+  {id: nil,name: I18n.t!("default_activity_documentation"), position: 5, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
 
 evl = TimeEntryActivity.seed(:id,
-  {id: 6,name: I18n.t!("default_activity_evaluation"), position: 6, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
+  {id: nil,name: I18n.t!("default_activity_evaluation"), position: 6, is_default: false, type: "TimeEntryActivity", active: true, project_id: nil, parent_id: nil, position_name: nil}).first
 
 activities = {default_activity_development: dev, default_activity_documentation: doc, default_activity_planification: pl,
     default_activity_evaluation: evl, default_activity_investigation: inv, default_activity_testing: tst}
