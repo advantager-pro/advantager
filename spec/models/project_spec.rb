b6f6f3e1b4434d2089ce26db33a416c77f251042
Edipo Vinicius da Silva <edipox@gmail.com>
2017-01-28 21:57:20 -0300

    [WIP] Add helper method to create issues

--- spec/models/project_spec.rb
+++ spec/models/project_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
+def create_issue(project, planned_value, previous_planned_value=0, date=nil)
+  issue = FactoryGirl.create(:issue, project: project)
+  issue.send("#{project.issue_evm_field}=", planned_value-previous_planned_value)
+  issue.due_date = date || Time.now
+  issue.start_date = date || Time.now
+  issue.save!
+  issue
+end
+
 class Float
   def round_to(x)
     (self * 10**x).round.to_f / 10**x
@@ -40,56 +49,14 @@ RSpec.describe Project, type: :model do
     #   8             2950
     #   9             3350
     " do
-      # set
-      # # month   BCWS (planned_value)     BCWP (earned_value)
-      #   5             1500                    1360
-      #   6             2000                    1830
-      #   7             2500                    2260
-      #   8             2950
-      #   9             3350
       it "returns the expected values" do
-        # 7 months from 01/07/2017:
-        #Default Issue status
-        # default_issue_status = IssueStatus.create(name: "Nueva", is_closed: false, position: 1, default_done_ratio: nil)
-        # default_issue_status.attributes = {name: "New", locale: :en}
-        # default_issue_status.attributes = {name: "Nueva", locale: :es}
-        # default_issue_status.save!
-        #
-        # pro =IssueStatus.create(name: "En progreso", is_closed: false, position: 2, default_done_ratio: nil)
-        # pro.attributes = {name: "In progress", locale: :en}
-        # pro.attributes = {name: "En progreso", locale: :es}
-        # pro.save!
-        #
-        # clo = IssueStatus.create(name: "Cerrada", is_closed: true, position: 3, default_done_ratio: 100)
-        # clo.attributes = {name: "Closed", locale: :en}
-        # clo.attributes = {name: "Cerrada", locale: :es}
-        # clo.save!
-        #
-        # rej = IssueStatus.create(name: "Rechazada", is_closed: false, position: 4, default_done_ratio: nil)
-        # rej.attributes = {name: "Rejected", locale: :en}
-        # rej.attributes = {name: "Rechazada", locale: :es}
-        # rej.save!
-        #
-        # mt = Tracker.create(name: "Hito", position: 1, is_in_roadmap: true, default_status_id: default_issue_status.id)
-        # mt.attributes = {name: "Milestone", locale: :en}
-        # mt.attributes = {name: "Hito", locale: :es}
-        # mt.save!
-        #
-        # ts = Tracker.create(name: "Tarea", position: 2, is_in_roadmap: true, default_status_id: default_issue_status.id)
-        # ts.attributes = {name: "Task", locale: :en}
-        # ts.attributes = {name: "Tarea", locale: :es}
-        # ts.save!
-
 
         Timecop.travel(initial_time)
         # start the project
 
-
         project = FactoryGirl.create(:project, evm_field: evm_field, evm_frequency: evm_frequency, created_on: Time.now)
         issue_evm_field = project.issue_evm_field
         entry_evm_field = project.entry_evm_field
-
-
 
         issue1 = FactoryGirl.create(:issue, project: project, start_date: Time.now)
         # issue1.start_date = Time.now
@@ -99,11 +66,7 @@ RSpec.describe Project, type: :model do
         # Go to month 5
         Timecop.travel(initial_time + (5*evm_frequency).days)
         # Set PV month 5
-        issue5 = FactoryGirl.create(:issue, project: project)
-        issue5.send("#{issue_evm_field}=", 1500)
-        issue5.due_date = Time.now
-        issue5.start_date = Time.now
-        issue5.save!
+        issue5 = create_issue(project, 1500)
         # set EV month 5
         entry5 = FactoryGirl.build(:time_entry, issue: issue5)
         # PV = 1500
@@ -113,83 +76,87 @@ RSpec.describe Project, type: :model do
         # 1500*x  = 1360 = ac
         entry5.send("#{entry_evm_field}=", 1360)
         entry5.save!
-        issue5.done_ratio = (1360.0/1500.0)*100
+        issue5.done_ratio = (1360.0*100.0)/ issue5.planned_value
+
+        # puts 
         issue5.save!
+        project.reload
+        expect(project.planned_value).to eq(1500)
+        expect(project.earned_value.round).to eq(1360)
+
         # Advantager::EVM::Point.generate_from_project_begining(project)
         # Go to month 6
         Timecop.travel(initial_time + (6*evm_frequency).days)
         # Set PV month 6
-        issue6 = FactoryGirl.create(:issue, project: project)
-        issue6.send("#{issue_evm_field}=", 2000-1500)
-        issue6.due_date = Time.now
-        issue6.start_date = Time.now
-        issue6.save!
+        issue6 = create_issue(project, 2000, 1500) 
+        
         # set EV month 6
         entry6 = FactoryGirl.build(:time_entry, issue: issue5)
         entry6.send("#{entry_evm_field}=", 1830-1360)
         entry6.save!
-        issue6.done_ratio = (entry6.send(entry_evm_field)/1830.0)*100
+        issue6.done_ratio = ((1830-1360)*100.0)/ issue6.planned_value
         issue6.save!
+
+        project.reload
+        expect(project.planned_value).to eq(2000)
+        expect(project.earned_value.round).to eq(1830)
+
         # Advantager::EVM::Point.generate_from_project_begining(project)
         # Go to month 7
         Timecop.travel(initial_time + (7*evm_frequency).days)
         # Set PV month 7
-        issue7 = FactoryGirl.create(:issue, project: project)
-        issue7.send("#{issue_evm_field}=", 2500-2000)
-        issue7.due_date = Time.now
-        issue7.start_date = Time.now
-        issue7.save!
+        issue7 = create_issue(project, 2500, 2000) 
         # set EV month 7
         entry7 = FactoryGirl.build(:time_entry, issue: issue5)
         entry7.send("#{entry_evm_field}=", 2260-1830)
         entry7.save!
-        issue7.done_ratio = (entry7.send(entry_evm_field)/2260.0)*100
+        issue7.done_ratio =  ((2260-1830)*100.0)/ issue7.planned_value # asdasddaadd ///////
         issue7.save!
+
+        project.reload
+        expect(project.planned_value).to eq(2500)
+        expect(project.earned_value.round).to eq(2260)
+
 
         # Set PV month 8
         month_8_date = initial_time + (8*evm_frequency).days
-        issue8 = FactoryGirl.create(:issue, project: project)
-        issue8.send("#{issue_evm_field}=", 2950-2500)
-        issue8.due_date = month_8_date
-        issue8.start_date = month_8_date
-        issue8.save!
+        issue8 = create_issue(project, 2950, 2500, month_8_date) 
+
+        project.reload
+        expect(project.planned_value).to eq(2950)
+
 
         # Set PV month 9
         month_9_date = initial_time + (9*evm_frequency).days
-        issue9 = FactoryGirl.create(:issue, project: project)
-        issue9.send("#{issue_evm_field}=", 3350-2950)
-        issue9.due_date = month_9_date
-        issue9.start_date = month_9_date
-        issue9.save!
+        issue9 = create_issue(project, 3350, 2950, month_9_date) 
+
+        project.reload
+        expect(project.planned_value).to eq(3350)
+
 
         # Set PV month 10
         month_10_date = initial_time + (10*evm_frequency).days
-        issue10 = FactoryGirl.create(:issue, project: project)
-        issue10.send("#{issue_evm_field}=", 3650-3350)
-        issue10.due_date = month_10_date
-        issue10.start_date = month_10_date
-        issue10.save!
+        issue10 = create_issue(project, 3650, 3350, month_10_date)
+
+        project.reload
+        expect(project.planned_value).to eq(3650)
+
 
         # Set PV month 11
         month_11_date = initial_time + (11*evm_frequency).days
-        issue11 = FactoryGirl.create(:issue, project: project)
-        issue11.send("#{issue_evm_field}=", 3900-3650)
-        issue11.due_date = month_11_date
-        issue11.start_date = month_11_date
-        issue11.save!
+        issue11 = create_issue(project, 3900, 3650, month_11_date) 
 
+        project.reload
+        expect(project.planned_value).to eq(3900)
 
                 # Set PV month 12
         month_12_date = initial_time + (12*evm_frequency).days
-        issue12 = FactoryGirl.create(:issue, project: project)
-        issue12.send("#{issue_evm_field}=", 4000-3900)
-        issue12.due_date = month_12_date
-        issue12.start_date = month_12_date
-        issue12.save!        
-        
-        
-        
+        issue12 = create_issue(project, 4000, 3900, month_12_date)
+   
+        project.reload
+        expect(project.planned_value).to eq(4000)
 
+     
         # go a bit after 7 months
         Timecop.travel(initial_time + (7*evm_frequency).days )#+ 1.day)
 
@@ -223,7 +190,7 @@ RSpec.describe Project, type: :model do
 
 
         expect(point.current_period).to eq(7)
-        expect(point.earned_schedule.ceil_to(2)).to eq(6.52)
+        expect(point.earned_schedule).to eq(6.52)
         expect(point.es_schedule_variance.ceil_to(2)).to eq(-0.48)
         expect(point.es_schedule_performance_index.round(2)).to eq(0.93)
         expect(point.es_independent_time_estimate_at_compete.ceil_to(1)).to eq(12.9)
