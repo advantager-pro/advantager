301feb21d9afa22a611cd73ff9b013bf9812273b
tania daniela paiva <taniadaniela1803@gmail.com>
2017-05-07 20:19:12 -0400

    Added menu and routes for default configs.

--- plugins/add_evm_links/config/routes.rb
+++ plugins/add_evm_links/config/routes.rb
@@ -1,3 +1,4 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 get 'projects/:project_id/evm_show', to: 'charts#evm_show'
+get 'default_configs', to: 'default_configs#show', as: :default_configs
--- plugins/add_evm_links/init.rb
+++ plugins/add_evm_links/init.rb
@@ -6,12 +6,15 @@ Redmine::Plugin.register :add_evm_links do
   url 'http://advantager.pro'
   author_url 'http://advantager.pro'
 
-  permission :view_evm_charts, { charts:  [:evm_show] }
+  permission :view_evm_charts,{ charts:  [:evm_show] }
 
-  menu :project_menu, :earned_value, { controller: 'charts', action: 'evm_show', copy_from: nil }, 
-    param: :project_id, caption: :evm_section, after: :overview
+  menu :project_menu, :earned_value,
+       { controller: 'charts', action: 'evm_show', copy_from: nil },
+       param: :project_id, caption: :evm_section, after: :overview
 
-  permission :set_default_configs, { default_configs: [:load] }
+  permission :set_default_configs, { default_configs: [:show] }
   
-  menu :admin_menu, ':default_configs', { controller: 'default_configs', action: 'load' }
+  menu :admin_menu, :default_configs,
+       { controller: 'default_configs', action: 'show' },
+       caption: :label_default_configs
 end
