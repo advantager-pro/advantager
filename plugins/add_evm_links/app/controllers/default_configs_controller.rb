8da45de9f70349c5d4a915d7e3d5002b42c89e08
tania daniela paiva <taniadaniela1803@gmail.com>
2017-05-09 21:51:28 -0400

    Added restrictions for executing restore default configs action.

--- plugins/add_evm_links/app/controllers/default_configs_controller.rb
+++ plugins/add_evm_links/app/controllers/default_configs_controller.rb
@@ -4,10 +4,24 @@ RedmineApp::Application.load_tasks
 class DefaultConfigsController < ApplicationController
 
   def show
+    @current_user = User.current
   end
 
   def load
-    Rake::Task['db:seed_fu'].execute
-    flash[:success] = "Default configs loaded." # add to locales
+    if check_current_user params[:username], params[:password]
+      Rake::Task['db:seed_fu'].execute
+      flash[:success] = "Default configs loaded." # add to locales
+    else
+      flash[:error] = "The credentials are wrong"
+    end
   end
+
+  private
+
+  def check_current_user username, password
+    current_user = User.current
+    return false if current_user.login != params[:username]
+    return if current_user.check_password?(password)
+  end
+
 end
