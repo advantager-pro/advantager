e317a31907d12c98989e81ba1f83f43665d9dcb4
Edipo Vinicius da Silva <edipox@gmail.com>
2017-05-30 08:11:34 -0400

    Add table check to settings initializer. Fix Earned schedule points generator

--- config/initializers/override_default_settings.rb
+++ config/initializers/override_default_settings.rb
@@ -1,4 +1,5 @@
 def override_default(name, value)
+  return unless ActiveRecord::Base.connection.table_exists? Setting.table_name
   Setting.send("#{name}=", value) if Setting.where(name: name).first.nil?
 end
 
