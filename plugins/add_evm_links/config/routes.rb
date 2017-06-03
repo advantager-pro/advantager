c20f42c669f3d72419fb4c3f62fc16992c279cb5
Edipo Vinicius da Silva <edipox@gmail.com>
2017-06-03 17:37:29 -0400

    Project list refactor

--- plugins/add_evm_links/config/routes.rb
+++ plugins/add_evm_links/config/routes.rb
@@ -1,5 +1,5 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
-get 'projects/:project_id/evm_show', to: 'charts#evm_show'
+get 'projects/:project_id/evm_show', to: 'charts#evm_show', as: 'project_evm'
 get 'default_configs', to: 'default_configs#show', as: :default_configs
 post 'load_default_configs', to: 'default_configs#load'
